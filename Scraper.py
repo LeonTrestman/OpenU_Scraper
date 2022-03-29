87da2d7c65ce175d5e156f75761235954f570b49
Leon Trestman <tleonleont@gmail.com>
2022-03-29 05:07:28 +0300

    -added get_user_course_info functionality

--- Scraper.py
+++ Scraper.py
@@ -1,23 +1,19 @@
-import requests
-
+import pandas as pd
 from Sheilta_Webdriver import Sheilta_Webdriver
-from utils import *
+from Table_Locations import USER_COURSE_INFO_TABLE_LOCATION
+from URLs import User_Course_Info_URL
+from utils import preprocess_grade_table
 
 
 class Scraper:
+
     def __init__(self):
-        session = self.get_logged_in_session()
+        self.session = Sheilta_Webdriver().get_logged_in_session()
 
-    def get_logged_in_session(self) -> requests.Session:
-        """Returns a logged in to sheilta session"""
-        session = requests.Session()
-        login_cookies = Sheilta_Webdriver().get_login_cookies()
-        session.cookies.update(login_cookies)
-        return session
+    def get_user_course_info(self) -> pd.DataFrame:
+        """Returns user course info as a dataframe"""
+        response = self.session.get(User_Course_Info_URL)
+        user_course_info_table = pd.read_html(response.content)[USER_COURSE_INFO_TABLE_LOCATION]
+        return preprocess_grade_table(user_course_info_table)
 
-
-
-
-
-
-leon = Scraper()
+    def get_
