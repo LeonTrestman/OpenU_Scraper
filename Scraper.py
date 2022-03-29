4298fae00cbf255471a4284df36f244c658fb752
Leon Trestman <tleonleont@gmail.com>
2022-03-29 03:39:43 +0300

    -start building the scraper

--- Scraper.py
+++ Scraper.py
@@ -1,2 +1,23 @@
+import requests
+
+from Sheilta_Webdriver import Sheilta_Webdriver
+from utils import *
+
+
 class Scraper:
- pass
+    def __init__(self):
+        session = self.get_logged_in_session()
+
+    def get_logged_in_session(self) -> requests.Session:
+        """Returns a logged in to sheilta session"""
+        session = requests.Session()
+        login_cookies = Sheilta_Webdriver().get_login_cookies()
+        session.cookies.update(login_cookies)
+        return session
+
+
+
+
+
+
+leon = Scraper()
